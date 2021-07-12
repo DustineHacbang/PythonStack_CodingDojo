684b3f75c6aa2574206ab7595f4ae74bf1e72a02
Dustine Hacbang <dustinehacbang526@gmail.com>
2021-07-12 13:50:28 -0700

    playground

--- Flask/Assignment_Fundamental/playground/server.py
+++ Flask/Assignment_Fundamental/playground/server.py
@@ -3,8 +3,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-    return render_template("index.html")
+@app.route("/play")
+@app.route("/play/<string:color>/<int:num>")
+def index(color = "red" ,num = 8):
+    return render_template("index.html", color = color,num = num)
+
+
 
 if __name__ == "__main__":
     app.run(debug = True)
