7bece8e90e3642e28a737cf8832591d288ad94a5
Dustine Hacbang <dustinehacbang526@gmail.com>
2021-07-13 15:07:27 -0700

    Counter

--- Flask/Assignment_Fundamental/counter/server.py
+++ Flask/Assignment_Fundamental/counter/server.py
@@ -1,14 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session
+from werkzeug.utils import redirect
 
 app = Flask(__name__)
+app.secret_key = "counting"
 
 @app.route("/")
 def index():
-    return render_template("index.html")
-
-@app.route("/")
-def index():
+    if "counter" in session:
+        session["counter"] += 1
+    else:
+        session["counter"] = 1
+    print(session["counter"])
     return render_template("counter.html")
+
+@app.route("/counter")
+def counter():
+    session["counter"] += 1
+    return redirect("/")
 
 if __name__ =="__main__":
     app.run(debug = True)
