ae7f5a4f6ed6462b254344491107c9ae0b146785
Dustine Hacbang <dustinehacbang526@gmail.com>
2021-08-04 08:21:49 -0700

    revised

--- Python/Assignment_Fundamental/function_basic_II.py
+++ Python/Assignment_Fundamental/function_basic_II.py
@@ -1,41 +1,62 @@
 1#
-countdown = list(range(0,6,1))
-print(countdown)
+def countdown(num): 
+    output = []
+    for i in range(num,-1,-1):
+        output.append(i)
+    return output
+print(countdown(5))
 
-countdown = []
-for i in range(0,6,1):
-    countdown.append(i)
-print(countdown)
+# countdown = list(range(0,6,1))
+# print(countdown)
+
+# countdown = []
+# for i in range(0,6,1):
+#     countdown.append(i)
+# print(countdown)
 
 
-countdown = [0,0,0,0,0]
-for i in range(0,6,1):
-    countdown[i]=i
-print(countdown)
+# countdown = [0,0,0,0,0]
+# for i in range(0,6,1):
+#     countdown[i]=i
+# print(countdown)
 
 
 2#
-list1 = [1,2]
-def print_and_return(i):    
-    print(i[0])
-    return(i[1])
+def print_and_return(list):    
+    print(list[0])
+    return (list[1])
 
-print_and_return(list1)
+print(print_and_return([1,2]))
 
 3#
-list1 = [1,2,3,4,5]
-def first_plus_length(i):
-    var = list1[1] +  len(list1)
-    print(var)
 
-first_plus_length(list1)
+def first_plus_length(list):
+    return list[0] + len(list)
+
+print(first_plus_length([1,2,3,4,5]))
 
 4#
 list1 = [5,2,3,2,1,4]
 
-def values_greater_than_second(i):
-    list2 = []
-    if list1[i] > 2
-    list2.append(list[i])
+def values_greater_than_second(list):
+    if len(list) < 2:
+        return False
+    output = []
+    for i in range(0,len(list)):
+        if list[i] > list[1]:
+            output.append(list[i])
+    print(len(output))
+    return output
+
+print(values_greater_than_second([5,2,3,2,1,4]))
+print(values_greater_than_second([3]))
 
 5#
+def length_and_value(size,value):
+    output = []
+    for i in range(0,size):
+        output.append(value)
+    return output
+
+print(length_and_value(4,7))
+print(length_and_value(6,2))
